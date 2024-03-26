aaacb0f2c2a2467b658d7a58f40dd7a4e33de9c3
Alexey-Sagaydak <alex230703@yandex.ru>
2024-03-26 12:48:12 +0700

    delete tests

--- tests/test.sh
+++
@@ -1,8 +0,0 @@
-ip="0.0.0.0:7777"
-
-user_data="{\"username\": \"admin\", \"password\": \"admin\", \"isAdmin\": true}"
-curl -X POST -H "Content-Type: application/json" -d "${user_data}" "http://${ip}/user"
-
-user_data="{\"username\": \"user\", \"password\": \"user\", \"isAdmin\": false}"
-curl -X POST -H "Content-Type: application/json" -d "${user_data}" "http://${ip}/user"
-
